ef051569c7d5b19b5f19ae57599df43ae4ed2bfc
WON SEOB SEO <won.seob.seo@zalando.fi>
2018-05-17 06:55:01 +0900

    Add libraries

--- project.clj
+++ project.clj
@@ -3,10 +3,16 @@
   :url "http://clojure-getting-started.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [compojure "1.6.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [environ "1.0.0"]]
+                 [environ "1.0.0"]
+                 [cheshire "5.6.0"]
+                 [clojure.java-time "0.3.2"]
+                 [com.taoensso/sente "1.12.0"]
+                 [http-kit "2.2.0"]
+                 [ring/ring-defaults "0.2.0"]
+                 [ring-cors "0.1.7"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
