acd7ac020f70d2e22dc8d9ff15ca36cd4f9e11eb
WON SEOB SEO <won.seob.seo@zalando.fi>
2018-05-17 07:22:49 +0900

    Delete dummy test

--- test/aikakone_backend/web_test.clj
+++
@@ -1,6 +0,0 @@
-(ns clojure-getting-started.web-test
-  (:require [clojure.test :refer :all]
-            [clojure-getting-started.core :refer :all]))
-
-(deftest first-test
-  (is false "Tests should be written"))
